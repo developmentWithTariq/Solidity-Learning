0437e8c2aaa67d3b35d325a7fd32126d8f48801e
Tariq15994 <tariqnawaz15994@gmail.com>
2022-02-24 17:06:30 +0500

    edited math and overload

--- solidityPractice/contracts/overload.sol
+++ solidityPractice/contracts/overload.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Overload {
+    /*
+    You can have multiple definitions for the same function name in the same scope. The definition of the 
+    function must differ from each other by the types and/or the number of arguments in the argument list.
+     You cannot overload function declarations that differ only by return type.
+     */
 
     function getSum(uint a, uint b) public pure returns(uint){      
        return a + b;
